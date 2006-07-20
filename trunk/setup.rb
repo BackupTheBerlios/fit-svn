b3850acf053af95b91807ce1b27939ddc7f5b8d6
gml <gml@d4f856ec-8418-0410-a8dd-c2dfc5fee33b>
2006-07-20 22:29:02 +0000

    setup.rb

--- trunk/setup.rb
+++ trunk/setup.rb
@@ -2,14 +2,13 @@
 #
 
 chdir_cmd=%{cd src/third_party/pcap}
-osx_cmd=%{mv extconf_ppc.rb extconf.rb; mv mkmf_ppc.rb mkmf.rb}
-pcap_cmd=%{ruby extconf.rb;make;make install}
 uname=`uname -p`.chomp!
 
 if uname.eql? "powerpc"
-	cmd="#{chdir_cmd};#{osx_cmd};#{pcap_cmd}"
+	pcap_cmd=%{ruby extconf_ppc.rb;make;make install}
 else
-	cmd="#{chdir_cmd};#{pcap_cmd}"
+	pcap_cmd=%{ruby extconf.rb;make;make install}
 end
+cmd="#{chdir_cmd};#{pcap_cmd}"
 system cmd
 
